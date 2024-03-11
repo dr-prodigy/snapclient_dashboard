8d9b060bb918effb55611ee21b5be15add640723
Maurizio Montel <dr.prodigy.github@gmail.com>
2024-03-11 01:18:46 +0100

    config_sample.py addition

---
+++ config_sample.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# LCD modes : NONE = 0, GPIO_CharLCD = 1, I2C = 2
+MODULE_LCD = 1
+I2C_BUS = 1  # i2c bus (0 -- original Pi, 1 -- Rev 2 Pi)
+I2C_ADDRESS = 0x27
+LCD_COLUMNS = 16
+LCD_ROWS = 2
+
+# LCD GPIO [RS, EN, D4, D5, D6, D7]
+GPIO_LCD = [4, 17, 22, 23, 24, 25]
+
+# Rotary encoder GPIO
+GPIO_ROTARY = [9, 10, 11]
+
+HASS_SERVER = 'https://myserver/'
+HASS_TOKEN = 'mykey'
+HASS_CHECK_SSL_CERT = False
+HASS_PLAYER_ENTITY_ID = 'media_player.my_player'
+
+# DEBUGGING
+TEST_MODE = 1
+DEBUG_LOG = True
+VERBOSE_LOG = False
+
+# VARIOUS SETTINGS
